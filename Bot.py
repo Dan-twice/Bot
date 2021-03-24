b824d3ea279c637f11a989255c505f86a4f6bb3f
Dan-twice <daniil.kalyagin@gmail.com>
2021-03-24 22:18:39 +0200

    eighth

--- Bot.py
+++ Bot.py
@@ -1,7 +1,10 @@
 import logging
 import sys
 from aiogram import Bot, Dispatcher, executor, types
+from flask import Flask
+import os
 
+app = Flask(__name__)
 API_TOKEN = '1750326917:AAGYFKRGQAwYuT0M-vw0SIam9uIgww2vwWc'
 
 # Configure logging
@@ -24,10 +27,13 @@ async def send_welcome(message: types.Message):
 async def echo(message: types.Message):
     # old style:
     # await bot.send_message(message.chat.id, message.text)
-    if message.text.lower() == 'buy':
+    if message.text.lower() == 'bay':
+        message.answer("Good bay, till next time!")
         sys.exit()
     await message.answer(message.text)
 
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
